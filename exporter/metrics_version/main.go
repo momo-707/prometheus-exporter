2af94e63a0cb306e3bcc794b14f0ebf904d2757c
momo-707 <64874423+momo-707@users.noreply.github.com>
2020-06-20 22:06:56 +0800

    Add files via upload

---
+++ exporter/metrics_version/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"example/metrics"
+)
+
+func main(){
+	http.HandleFunc("/abc", index)
+	http.Handle("/metrics", promhttp.Handler())
+	metrics.Register()
+	err := http.ListenAndServe(":5565", nil) // 设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	timer:=metrics.NewAdmissionLatency()
+	metrics.RequestIncrease()
+	num:=os.Getenv("Num")
+	if num==""{
+		Num_all(20)
+		_,err:=w.Write([]byte("there is no env Num. Computation successed\n"))
+		if err!=nil{
+			log.Println("err:"+err.Error()+" No\n")
+		}
+	}else{
+		numInt,_:=strconv.Atoi(num)
+		Num_all(numInt)
+		_,err:=w.Write([]byte("there is env Num. Computation successed\n"))
+		if err!=nil{
+			log.Println("err:"+err.Error()+" Yes\n")
+		}
+	}
+	timer.Observe()
+}
+
+func Num_all(n int)int{
+	return ((1+n)*n)/2;
+}
